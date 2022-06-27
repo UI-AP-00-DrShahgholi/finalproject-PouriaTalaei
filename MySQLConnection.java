ce93f5a14af6dfebea7b70148284c39161deccf8
PouriaTalaei <talaei.pouria06@gmmai.com>
2022-06-27 21:30:55 +0430

    new

--- MySQLConnection.java
+++ MySQLConnection.java
@@ -7,9 +7,9 @@ public class MySQLConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration", "root", "@Pouria1381");
-            String sqlCom = String.format("INSERT INTO people (ID,NationalCode,Name,Age,Sex,DigitalWallet) values (%s,%s,'%s',%s,'%s',%s)", id, nationalCode, name, age, sex, wallet);
-            Statement statement = connection.prepareStatement(sqlCom);
-            statement.execute(sqlCom);
+          //  String sqlCom = String.format("INSERT INTO people (ID,NationalCode,Name,Age,Sex,DigitalWallet) values (%s,%s,'%s',%s,'%s',%s)", id, nationalCode, name, age, sex, wallet);
+          //  Statement statement = connection.prepareStatement(sqlCom);
+            //statement.execute(sqlCom);
             connection.close();
             return true;
         }catch (Exception e){
