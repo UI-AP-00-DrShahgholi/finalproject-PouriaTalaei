843904dcb09e821029b4523a6d2cb2b0f56ccf3c
PouriaTalaei <talaei.pouria06@gmmai.com>
2022-06-28 16:13:24 +0430

    update & delete people

--- MySQLConnection.java
+++ MySQLConnection.java
@@ -1,19 +1,17 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MySQLConnection {
 
-    public static Connection connectionSql() throws Exception{
+    public static Connection connectionSql() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration", "root", "@Pouria1381");
         return connection;
     }
 
-    public static boolean executeSQL(Connection connection,String sqlCmd) /*throws Exception*/ {
+    public static boolean executeSQL(Connection connection, String sqlCmd) /*throws Exception*/ {
 //        try {
-          //  Class.forName("com.mysql.cj.jdbc.Driver");
-            //Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration", "root", "@Pouria1381");
+        //  Class.forName("com.mysql.cj.jdbc.Driver");
+        //Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration", "root", "@Pouria1381");
 
         // String sqlCom = String.format("INSERT INTO people (ID,NationalCode,Name,Age,Sex,DigitalWallet) values (%s,%s,'%s',%s,'%s',%s)", 100, 200, "pouria", 20, "male" , 5000000);
 
@@ -22,8 +20,31 @@ public class MySQLConnection {
             statement.execute(sqlCmd);
             connection.close();
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
+    }
+
+    public static ResultSet executeQuerySQL(Connection connection, String sqlCmd) /*throws Exception*/ {
+        try {
+            Statement statement = connection.prepareStatement(sqlCmd);
+            ResultSet resultSet = statement.executeQuery(sqlCmd);
+            while (resultSet.next()) {
+
+
+            }
+
+            return resultSet;
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    int getMaxID() throws SQLException {
+        String sqlCmd = "Select Max(ID) from people";
+
+
+
     }
 }
--- SqlConnectionMethods.java
+++ SqlConnectionMethods.java
@@ -1,13 +1,37 @@
 import java.sql.Connection;
 
 public class SqlConnectionMethods {
-    public void insertPeople(int iD,int nationalCode,int name,int age,int sex,int digitalWallet) throws Exception {
+    public void insertPeople(int iD, int nationalCode, int name, int age, int sex, int digitalWallet) throws Exception {
         Connection connection = MySQLConnection.connectionSql();
-        String sqlCmd = String.format("INSERT INTO people (ID,NationalCode,Name,Age,Sex,DigitalWallet) values (%s,%s,'%s',%s,'%s',%s)", iD, nationalCode, name, age, sex , digitalWallet);
-        if(MySQLConnection.executeSQL(connection,sqlCmd))
+        String sqlCmd = String.format("INSERT INTO people (ID,NationalCode,Name,Age,Sex,DigitalWallet) values (%s,%s,'%s',%s,'%s',%s)", iD, nationalCode, name, age, sex, digitalWallet);
+        if (MySQLConnection.executeSQL(connection, sqlCmd))
             System.out.println("insert successfully!");
         else
             System.out.println("insert not successfully!");
+
+    }
+
+    public void updatePeople(int iD, int nationalCode, int name, int age, int sex, int digitalWallet) throws Exception {
+        Connection connection = MySQLConnection.connectionSql();
+        String sqlCmd = String.format("UPDATE people SET NationalCode=%s , Name ='%s',Age =%s,Sex='%s',DigitalWallet=%s WHERE ID = %s", nationalCode, name, age, sex, digitalWallet, iD);
+        if (MySQLConnection.executeSQL(connection, sqlCmd))
+            System.out.println("Update successfully!");
+        else
+            System.out.println("update not successfully!");
+    }
+
+    public void deletePeople(int iD) throws Exception {
+        Connection connection = MySQLConnection.connectionSql();
+        String sqlCmd = String.format("DELETE from people WHERE ID = %s",iD);
+        if (MySQLConnection.executeSQL(connection, sqlCmd))
+            System.out.println("Delete successfully!");
+        else
+            System.out.println("Delete not successfully!");
+    }
+
+
+    public void getMaxID() throws Exception {
+
 
     }
 
