97f554a4dae70ced7bca2b0e29defbdb2df82121
PouriaTalaei <talaei.pouria06@gmmai.com>
2022-06-30 03:53:03 +0430

    insert update & load account

---
+++ Account.java
@@ -0,0 +1,61 @@
+public class Account {
+    private String accountNumber;
+    private String ownerNationalCode;
+    private int balance;
+    private String dateOfOpeningAccount;
+    private int negativePoint;
+
+
+    public Account(String accountNumber, String ownerNationalCode, int balance, String dateOfOpeningAccount) {
+        this.accountNumber = accountNumber;
+        this.ownerNationalCode = ownerNationalCode;
+        this.balance = balance;
+        this.dateOfOpeningAccount = dateOfOpeningAccount;
+        negativePoint = 0;
+    }
+
+    public Account() {
+
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwnerNationalCode() {
+        return ownerNationalCode;
+    }
+
+    public void setOwnerNationalCode(String ownerNationalCode) {
+        this.ownerNationalCode = ownerNationalCode;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getDateOfOpeningAccount() {
+        return dateOfOpeningAccount;
+    }
+
+    public void setDateOfOpeningAccount(String dateOfOpeningAccount) {
+        this.dateOfOpeningAccount = dateOfOpeningAccount;
+    }
+
+    public int getNegativePoint() {
+        return negativePoint;
+    }
+
+    public void setNegativePoint(int negativePoint) {
+        this.negativePoint = negativePoint;
+    }
+
+}
---
+++ UserManagement.java
@@ -0,0 +1,69 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserManagement {
+
+
+    public void insertAccounts(String accountNumber, String ownerNationalCode, int balance, String dateOfOpeningAccount, int negativePoint) throws Exception {
+        Connection connection = MySQLConnection.connectionSql();
+        String sqlCmd = String.format("INSERT INTO accounts (ID,AccountNumber,OwnerNationalCode,Balance,DateOfOpeningAccount,NegativePoint) values (%s,'%s','%s',%s,'%s',%s)", getMaxAccountsID() + 1, accountNumber, ownerNationalCode, balance, dateOfOpeningAccount, negativePoint);
+        if (MySQLConnection.executeSQL(connection, sqlCmd))
+            System.out.println("insert successfully!");
+        else
+            System.out.println("insert not successfully!");
+    }
+
+    public void updateAccounts(int iD, String accountNumber, String ownerNationalCode, int balance, String dateOfOpeningAccount, int negativePoint) throws Exception {
+        Connection connection = MySQLConnection.connectionSql();
+        String sqlCmd = String.format("UPDATE accounts SET AccountNumber='%s' , OwnerNationalCode ='%s',Balance =%s,DateOfOpeningAccount='%s',NegativePoint=%s WHERE ID = %s", accountNumber, ownerNationalCode, balance, dateOfOpeningAccount, negativePoint, iD);
+        if (MySQLConnection.executeSQL(connection, sqlCmd))
+            System.out.println("Update successfully!");
+        else
+            System.out.println("Update not successfully!");
+    }
+
+
+    public Account loadAccount(int iD) throws Exception {
+        Connection connection = MySQLConnection.connectionSql();
+        String sqlCmd = String.format("SELECT * from accounts WHERE ID =%s", iD);
+        ResultSet resultSet = MySQLConnection.executeQuerySQL(connection, sqlCmd);
+        Account account = new Account();
+        account = setAccount(resultSet);
+        return account;
+
+        //printPerson(person);
+
+    }
+
+    public Account setAccount(ResultSet resultSet) throws SQLException {
+        Account account;
+        account = new Account();
+        account.setAccountNumber(resultSet.getString("AccountNumber"));
+        account.setOwnerNationalCode(resultSet.getString("OwnerNationalCode"));
+        account.setBalance(Integer.parseInt(resultSet.getString("Balance")));
+        account.setDateOfOpeningAccount(resultSet.getString("DateOfOpeningAccount"));
+        account.setNegativePoint(Integer.parseInt(resultSet.getString("NegativePoint")));
+        return account;
+    }
+
+    public void printAccount(Account account) {
+        System.out.println("Account Number : " + account.getAccountNumber());
+        System.out.println("Owner National Code : " + account.getOwnerNationalCode());
+        System.out.println("Balance : " + account.getBalance());
+        System.out.println("Date Of Opening Account : " + account.getDateOfOpeningAccount());
+        System.out.println("Negative Point : " + account.getNegativePoint());
+    }
+
+
+    public int getMaxAccountsID() throws Exception {
+        Connection connection = MySQLConnection.connectionSql();
+        String sqlCmd = "SELECT MAX(ID) from accounts";
+        ResultSet resultSet = MySQLConnection.executeQuerySQL(connection, sqlCmd);
+        if (resultSet.next()) {
+            return resultSet.getInt(1);
+        } else return 0;
+    }
+
+//String accountNumber, String ownerNationalCode, int balance, String dateOfOpeningAccount)
+}
